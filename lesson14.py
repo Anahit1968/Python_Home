84e197c033d6d839c2ea7f11e64f8ce628712e32
Anahit1968 <akochinjan@mail.ru>
2022-07-01 16:02:46 +0400

    mb

--- lesson14.py
+++ lesson14.py
@@ -1,1 +1,10 @@
-print("Hello Word!")
+# def anun():
+#     print("Hello!")
+# anun()
+
+def func1(*str(x), count = 0):
+    for i in x:
+        count += 1
+    return count
+print(func1(a, b, c, 1, 2, 3))    
+
